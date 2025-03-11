f8a86f723b537e60b9e28a5e7e3021a32ad1527a
rayray39 <raynerlimfy@gmail.com>
2025-03-11 22:08:01 +0800

    App.tsx: Update date picker to be a controlled component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
+import { Button, Stack } from '@mui/material';
 import './App.css'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { Dayjs } from 'dayjs';
+import { useState } from 'react';
 
 
 function App() {
+    const [dateSelected, setDateSelected] = useState<Dayjs | null>(null);
+
+    const handleButtonClick = () => {
+        console.log('day selected ' + dateSelected?.date());
+        if (dateSelected?.month()) {
+            console.log('month selected ' + (dateSelected.month() + 1));
+        }
+        console.log('year selected ' + dateSelected?.year());
+        console.log(dateSelected);
+    }
+
     return (
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DatePicker />
-        </LocalizationProvider>
+        <Stack>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DatePicker value={dateSelected} onChange={(newValue) => setDateSelected(newValue)}/>
+            </LocalizationProvider>
+            <Button onClick={handleButtonClick}>click me</Button>
+        </Stack>
     );
 }
 
