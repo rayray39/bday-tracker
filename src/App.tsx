5d81b478118e6f352fe89b6f5f4928a7af3422cc
rayray39 <raynerlimfy@gmail.com>
2025-03-11 21:51:39 +0800

    App.tsx: Add date picker from material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import './App.css'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+
 
 function App() {
-  return (
-    <>
-      
-    </>
-  )
+    return (
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker />
+        </LocalizationProvider>
+    );
 }
 
 export default App
