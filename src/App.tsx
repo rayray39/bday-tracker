2fa0a63e03da30ce6db92ac21759bafb31ea2f47
rayray39 <raynerlimfy@gmail.com>
2025-03-12 16:55:18 +0800

    App.tsx: add textfield for name and display name-bday to UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Button, Stack } from '@mui/material';
+import { Box, Button, Stack, TextField } from '@mui/material';
 import './App.css'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
@@ -8,24 +8,51 @@ import { useState } from 'react';
 
 
 function App() {
+    // the date that has been selected through the date picker
     const [dateSelected, setDateSelected] = useState<Dayjs | null>(null);
+    // the name that has been entered into the textfield
+    const [nameEntered, setNameEntered] = useState<string>('');
+    // the list of dates that have been selected and saved
+    const [datesSaved, setDatesSaved] = useState<string[]>([]);
+    // the list of names that have been saved
+    const [names, setNames] = useState<string[]>([]);
 
     const handleButtonClick = () => {
-        console.log('day selected ' + dateSelected?.date());
-        if (dateSelected?.month()) {
-            console.log('month selected ' + (dateSelected.month() + 1));
+        console.log(dateSelected?.format('DD-MMMM-YYYY'));
+        console.log("name = " + nameEntered);
+        if (dateSelected && nameEntered) {
+            // append the new date and new name into dateSaved array and names array 
+            setDatesSaved(prev => [...prev, dateSelected.format('DD-MMMM-YYYY')]);
+            setNames(prev => [...prev, nameEntered]);
         }
-        console.log('year selected ' + dateSelected?.year());
-        console.log(dateSelected);
+        // clear the fields
+        setDateSelected(null);
+        setNameEntered('');
     }
 
     return (
-        <Stack>
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <DatePicker value={dateSelected} onChange={(newValue) => setDateSelected(newValue)}/>
-            </LocalizationProvider>
-            <Button onClick={handleButtonClick}>click me</Button>
-        </Stack>
+        <Box sx={{
+            display:'flex',
+            flexDirection:'column',
+            alignItems:'center',
+            margin:'auto',
+            marginTop:'80px',
+            overflowX:'hidden'
+        }}>
+            <Stack spacing={2}>
+            <TextField id="filled-basic" label="Enter name" variant="filled" value={nameEntered} onChange={(event) => setNameEntered(event.target.value)}/>
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DatePicker sx={{width:'400px'}} value={dateSelected} onChange={(newValue) => setDateSelected(newValue)}/>
+                </LocalizationProvider>
+                <Button sx={{width:'400px'}} variant='contained' onClick={handleButtonClick}>Add Bday</Button>
+            </Stack>
+
+            <Stack>
+                {datesSaved.map((date, index) => (
+                    <Box key={index}>{names[index]}-{date}</Box>
+                ))}
+            </Stack>
+        </Box>
     );
 }
 
